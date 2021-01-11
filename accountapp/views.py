fc936ecfe5f4bf181cbf6e4958029a2e2718beca
PKHong <pyungkang1004@gmail.com>
2021-01-12 04:04:18 +0900

    login logout

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+
+
+# Create Your views here
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -10,15 +16,30 @@ def hello_world(request):
 
     if request.method == "POST":
 
-        temp =request.POST.get('hello_world_input')
+        temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
+        # hello_world_list = HelloWorld.objects.all()
         # 리프레쉬를 해도 돌아가지 않겠금 하는거
+        # 리버스틑 함수용
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    #리버스 레이지는 클래스에서만 사용한다
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
+
+
+
+
+
