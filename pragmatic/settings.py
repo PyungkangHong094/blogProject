bba707d6e9985fc07567cfe37c25035ee6f16c4a
PKHong <pyungkang1004@gmail.com>
2021-01-14 02:00:46 +0900

    profile image and method decorator

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -149,6 +149,6 @@ LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 
 
-STATIC_URL = '/media/'
+MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
