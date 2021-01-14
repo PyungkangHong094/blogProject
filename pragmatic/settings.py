9feb5253b61bdc6ab2d40974da270ff3cdfd3d61
PKHong <pyungkang1004@gmail.com>
2021-01-14 22:26:04 +0900

    mobile add

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -36,12 +36,12 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
