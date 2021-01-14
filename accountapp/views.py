0d1d398c6073796422885c7b9db8da63d8b872ab
PKHong <pyungkang1004@gmail.com>
2021-01-15 01:33:21 +0900

    update list_fragment and MultipleObjectMixmin used

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,12 +10,15 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 # 배열로 만든다
+from articleapp.models import Article
+
 has_ownership = [account_ownership_required, login_required]
 
 @login_required
@@ -49,10 +52,17 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
 
     # def get(self, *args, **kwargs):
     #     if self.request.user.is_authenticated and self.get_object() == self.request.user:
