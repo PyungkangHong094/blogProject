2fedf9548091244aba4f595d3723fed43e870bcd
PKHong <pyungkang1004@gmail.com>
2021-01-17 17:11:29 +0900

    update subscribe button and function

---
+++ subscribeapp/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
+
+from projectapp.models import Project
+from subscribeapp.models import Subscription
+
+
+@method_decorator(login_required, 'get')
+class SubscriptionView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail', kwargs={'pk': self.request.GET.get('project_pk')})
+
+    def get(self, request, *args, **kwargs):
+        #단축 함수 프로젝트 피케이를 가지고있는 프로젝트를 찾는데 그게 없다면 404 라고 뜨는거다 :없으면 바로 되돌아가는
+        project = get_object_or_404(Project, pk=self.request.GET.get('project_pk'))
+        user = self.request.user
+
+        subscription = Subscription.objects.filter(user=user, project=project)
+
+        if subscription.exists():
+            subscription.delete()
+        else:
+            Subscription(user=user, project=project).save()
+        return super(SubscriptionView, self).get(request, *args, **kwargs)
