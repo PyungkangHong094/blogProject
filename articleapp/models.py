765a40a7b40661035cc9840cad7d7ac02bca8138
PKHong <pyungkang1004@gmail.com>
2021-02-02 15:20:28 +0900

    -fixing change the model

--- articleapp/models.py
+++ articleapp/models.py
@@ -15,7 +15,7 @@ class Article(models.Model):
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
 
-    created_at = models.DateField(auto_now_add=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
 
     def __str__(self):
